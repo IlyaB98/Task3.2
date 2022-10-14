55d07c871a300adba2a3cffe8d98cbd5e1c76d53
Ilya <b.ilya@mail.ru>
2022-10-14 17:33:29 +0300

    Выполнил дз полиморфизм

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import transport.Bus;
+import transport.Driver;
+import transport.PassengerCar;
+import transport.Truck;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PassengerCar lada = new PassengerCar("Lada", "Grande", 1.7);
+        System.out.println(lada);
+        PassengerCar audi = new PassengerCar("Audi", "A8 50 L TDI quattro", 3.0);
+        System.out.println(audi);
+        PassengerCar bmw = new PassengerCar("BMW", "Z8", 3.0);
+        System.out.println(bmw);
+
+        System.out.println();
+        Truck truck1 = new Truck("Truck-1", "A1", 4.0);
+        System.out.println(truck1);
+        Truck truck2 = new Truck("Truck-2", "A2", 4.5);
+        System.out.println(truck2);
+        Truck truck3 = new Truck("Truck-3", "A3", 3.0);
+        System.out.println(truck3);
+
+        System.out.println();
+        Bus bus1 = new Bus("Автобус-1", "B1", 2020);
+        System.out.println(bus1);
+        Bus bus2 = new Bus("Автобус-2", "B2", 2021);
+        System.out.println(bus2);
+        Bus bus3 = new Bus("Автобус-3", "B3", 2016);
+        System.out.println(bus3);
+
+        System.out.println();
+        Driver<PassengerCar> vladimir = new Driver<>("Владимир");
+        vladimir.go(audi);
     }
 }
---
+++ src/transport/Transport.java
@@ -0,0 +1,56 @@
+package transport;
+
+public abstract class Transport {
+    private String brand;
+    private String model;
+    private double engineVolume;
+
+    public Transport(String brand, String model, double engineVolume) {
+        setBrand(brand);
+        setModel(model);
+        setEngineVolume(engineVolume);
+    }
+
+    public abstract void start();
+    public abstract void stop();
+    public void setBrand(String brand) {
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+    }
+
+    public void setModel(String model) {
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.0;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    @Override
+    public String toString() {
+        return getBrand() + " " + getModel() + ", объем двигателя " + getEngineVolume();
+    }
+}
